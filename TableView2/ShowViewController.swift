4f27281ae87100a6e0fc8a7246ec85a387264d06
Forrest Filler <forrestfiller@users.noreply.github.com>
2016-06-18 15:28:03 -0400

    Trying to reconcile Stuart's reducing function suggestion  with his previous fix that works

--- TableView2/ShowViewController.swift
+++ TableView2/ShowViewController.swift
@@ -18,7 +18,6 @@ class ShowViewController: UIViewController {
         let view = UIView(frame: frame)
         view.backgroundColor = .darkGrayColor()
         
-        
         let showBackdrop = UIImageView(frame: CGRect(x: 0, y: 0, width: frame.size.height, height: frame.size.height)) // want a square so use the same height and width!
         showBackdrop.image = UIImage(named: self.show.image)
         view.addSubview(showBackdrop)
@@ -28,7 +27,8 @@ class ShowViewController: UIViewController {
         view.addSubview(darkScreen)
         
         let showImage = UIImageView(frame: CGRect(x: 0, y: 120, width: 160, height: 160))
-        showImage.center = CGPointMake(0.5*frame.size.width, showImage.center.y) // center property
+        showImage.center = CGPointMake(0.5*frame.size.width, showImage.center.y)
+        // center property
         showImage.layer.cornerRadius = 0.5*showImage.frame.size.height
         showImage.layer.masksToBounds = true
         showImage.layer.borderColor = UIColor.whiteColor().CGColor
@@ -37,18 +37,18 @@ class ShowViewController: UIViewController {
         view.addSubview(showImage)
         
         let font = UIFont(name: "Arial", size: 16)
-        
         let str = NSString(string: self.show.summary)
-//        let strCast = Array<String>(self.show.cast)
-//        // Need to figure out what type of class is an array of strings-- that is compatible with the boundingRectWithSize below!
-//        
-//        var boundsCast = strCast.boundingRectWithSize(CGSizeMake(frame.size.width-50, 510),
-//            options: .UsesLinesFragmentOrigin,
-//            attributes: [NSFontAttributeName:font!],
-//            context: nil)
-//        bounds.origin.x = 30
-//        bounds.origin.y = 310
-//        
+/*        let strCast = Array<String>(self.show.cast)
+// @DanKwon: using what we learned in class, I still need to figure out what type of class is an array 
+        of strings-- that is also compatible with the boundingRectWithSize below!
+        
+        var boundsCast = strCast.boundingRectWithSize(CGSizeMake(frame.size.width-50, 510),
+            options: .UsesLinesFragmentOrigin,
+            attributes: [NSFontAttributeName:font!],
+            context: nil)
+        bounds.origin.x = 30
+        bounds.origin.y = 310
+*/
         var bounds = str.boundingRectWithSize(
             CGSizeMake(frame.size.width-40, 500),
             options: .UsesLineFragmentOrigin,
@@ -56,16 +56,7 @@ class ShowViewController: UIViewController {
             context: nil)
         bounds.origin.x = 20
         bounds.origin.y = 300
-//
-//        let castLabel = UILabel(frame: bounds)
-//        castLabel.textColor = .whiteColor()
-//        castLabel.backgroundColor = .clearColor()
-//        castLabel.numberOfLines = 0
-//        castLabel.lineBreakMode = .ByWordWrapping
-//        castLabel.textAlignment = .Center
-//        castLabel.text = self.show.cast
-//        view.addSubview(castLabel)
-//        
+
         let summaryLabel = UILabel(frame: bounds)
         summaryLabel.textColor = .whiteColor()
         summaryLabel.backgroundColor = .clearColor()
@@ -74,9 +65,11 @@ class ShowViewController: UIViewController {
         summaryLabel.textAlignment = .Center
         summaryLabel.text = self.show.summary
         view.addSubview(summaryLabel)
+        
         var castString = ""
         for s in show.cast {
             castString += "\(s),"
+// @DanKwon:   ---Stuart suggested this block above and the block below this one called "castLabel" to sove my issue with Array<String> from the Show.swif file.
         }
         let castLabel = UILabel(frame: bounds)
         castLabel.textColor = .whiteColor()
